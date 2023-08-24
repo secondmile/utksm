c2cbdc65281ddbd63a85610fb35e408b819b4a7e
Becky Pennock <beckypennock@gmail.com>
2023-08-24 09:49:49 -0700

    Huston, we have displayed dynamic data!

--- src/assets/js/gformAjaxSubmit.js
+++ src/assets/js/gformAjaxSubmit.js
@@ -22,6 +22,7 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
     stateField.addEventListener('change', captureAndDisplaySelections);
     countryField.addEventListener('change', captureAndDisplaySelections);
 
+    // Interaction handler (capture selections, update results)
     function captureAndDisplaySelections() {
         // Capture the selected options from the form fields
         const selectedSpecialization = getSelectedRadio(specializationRadios);
@@ -34,18 +35,22 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
         if (selectedState) selectedFilters.push(selectedState);
         if (selectedCountry) selectedFilters.push(selectedCountry);
     
-        // Build the display content
-        const displayContent = `Specialization: ${selectedSpecialization ? selectedSpecialization.label : ''}<br>State: ${selectedState ? selectedState.label : ''}<br>Country: ${selectedCountry ? selectedCountry.label : ''}`;
+        // Build the display content (DEBUG/TESTING, delete later)
+        // const displayContent = `Specialization: ${selectedSpecialization ? selectedSpecialization.label : ''}<br>State: ${selectedState ? selectedState.label : ''}<br>Country: ${selectedCountry ? selectedCountry.label : ''}`;
     
+        // Fetch the updated content for Query Loop block
+        updateQueryLoopBlockContent(selectedFilters);
+
         // Apply fade effect to display the content
-        fadeOutAndIn(display, displayContent);
+        // fadeOutAndIn(display, updatedQueryLoopContent);
     
         // Fetch and update the Query Loop block content
-        updateQueryLoopBlockContent(selectedFilters);
+        // updateQueryLoopBlockContent(selectedFilters);
     
         // console.log(selectedFilters);
     }
     
+    // Dropdown Selection handling
     function getSelectedOption(selectField) {
         const selectedOption = selectField.querySelector(`option[value="${selectField.value}"]`);
         const selectedLabel = selectedOption ? selectedOption.textContent : '';
@@ -65,6 +70,7 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
         return null;
     }
     
+    // Radio selection handling
     function getSelectedRadio(radioButtons) {
         const selectedRadio = Array.from(radioButtons).find(radio => radio.checked);
     
@@ -81,11 +87,13 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
         return null;
     }
     
+    // Select parent ID containing keys
     function getParentFieldId(element) {
         const parentField = element.closest('[id^="field_"]');
         return parentField ? parentField.id : '';
     }
     
+    // Extract the key from that parent class for key:value matching for our endpoint params
     function getExtractedKeyFromClass(parentFieldId) {
         const parentField = document.getElementById(parentFieldId);
         const labelClass = Array.from(parentField.classList).find(className =>
@@ -100,9 +108,8 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
     
         return '';
     }
-    
-    
 
+    // Clears form field data so they don't stack up
     function clearFields(...fields) {
         fields.forEach(field => {
             field.value = '';
@@ -116,6 +123,20 @@ function setupAjaxFormSubmission(formSelector, displaySelector) {
             element.style.opacity = 1;
         }, 300);
     }
+}
+
+// Function to update Query Loop block content using AJAX
+function updateQueryLoopBlockContentWithAjax(apiURL) {
+    // Make an AJAX request to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', apiURL, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Update the target element with the received content
+            document.querySelector('.sm--counselor-query-block').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
 }
 
 // Function to update Query Loop block content
@@ -139,40 +160,9 @@ function updateQueryLoopBlockContent(filters) {
 
     console.log('API URL:', apiUrl);
 
-    fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('API request failed');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Debugging: Output the data received from the API
-        console.log('API Data:', data);
-
-        // Dispatch the data if it's available and properly structured
-        if (data) {
-            wp.data.dispatch('core/block-editor').updateBlockAttributes(queryLoopBlockId, {
-                // counselor_title: data.counselor-title,
-                // counselor_slate_url: data.counselor_slate_url,
-                // counselor_image: data.counselor_image,
-                // counselor_country: data.counselor_country,
-                // counselor_states: data.counselor_states,
-                // counselor_county: data.counselor_county,
-                // counselor_schools: data.counselor_schools,
-                counselor_specialization: data.counselor-specialization,
-            });
-        } else {
-            console.error('API response does not have expected structure.');
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
-
-
+    // Call the AJAX function with the constructed API URL
+    updateQueryLoopBlockContentWithAjax(apiUrl);
 }
 
-
 // Usage: Call the function with appropriate selectors
-setupAjaxFormSubmission('.smm-gform__counselor_wrapper', '.counselor-selection-display');
+setupAjaxFormSubmission('.smm-gform__counselor_wrapper', '.sm--counselor-query-block');
