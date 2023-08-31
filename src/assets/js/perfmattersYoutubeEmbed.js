ff30fe376f9579521db94c6fe3b14d992e089e11
Becky Pennock <beckypennock@gmail.com>
2023-08-31 15:07:42 -0700

    Made it so the lightbox overlay respects multiple elements, included the counselor query block styles i forgot to add from yesterday

--- src/assets/js/perfmattersYoutubeEmbed.js
+++ src/assets/js/perfmattersYoutubeEmbed.js
@@ -1,27 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const perfmattersLazyYouTube = document.querySelector('.perfmatters-lazy-youtube');
     const lightboxOverlay = document.createElement('div');
     lightboxOverlay.classList.add('lightbox-overlay');
+    document.body.appendChild(lightboxOverlay);
   
-    // perfmattersLazyYouTube.classList.add('lightbox-content');
-
-    // const lightboxContent = document.createElement('div');
-    // lightboxContent.classList.add('lightbox-content');
-    // lightboxContent.innerHTML = '<p>Lorem ipsum</p>'; // Optional content if that comes up
-  
-    // lightboxOverlay.appendChild(lightboxContent);
-    document.body.appendChild(lightboxOverlay); // Append to the body
-  
-    perfmattersLazyYouTube.addEventListener('click', function (event) {
-      event.preventDefault(); // Prevent default click behavior
-  
-      lightboxOverlay.style.display = 'block'; // Show the lightbox overlay
-      document.body.style.overflow = 'hidden'; // Prevent scrolling in the background
+    const perfmattersLazyYouTubes = document.querySelectorAll('.perfmatters-lazy-youtube');
+    
+    perfmattersLazyYouTubes.forEach(function(perfmattersLazyYouTube) {
+      perfmattersLazyYouTube.addEventListener('click', function (event) {
+        event.preventDefault();
+        perfmattersLazyYouTube.style.zIndex = 1000; // Set z-index for the clicked element
+        lightboxOverlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+      });
     });
   
     lightboxOverlay.addEventListener('click', function () {
-      lightboxOverlay.style.display = 'none';  // Hide the lightbox overlay
-      document.body.style.overflow = 'auto';    // Restore scrolling
+      perfmattersLazyYouTubes.forEach(function(perfmattersLazyYouTube) {
+        perfmattersLazyYouTube.style.zIndex = ''; // Remove z-index from all elements
+      });
+      lightboxOverlay.style.display = 'none';
+      document.body.style.overflow = 'auto';
     });
   });
   
