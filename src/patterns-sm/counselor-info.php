45c4e1cb039868a1bda2273f5d8ff952e75040ff
Becky Pennock <beckypennock@gmail.com>
2023-10-05 11:13:58 -0700

    Counselor Single | Added default fallback image, added check for page scope (was breaking site) and added schools to list

--- src/patterns-sm/counselor-info.php
+++ src/patterns-sm/counselor-info.php
@@ -15,32 +15,37 @@
 
 <?php
 
-function get_acf_fields_by_type($post_id = null) {
-    $acf_fields = get_fields($post_id); // Get all ACF fields for the post
-
-    $fields_by_type = array(
-        'string' => array(),
-        'number' => array(),
-        'array' => array(),
-    );
-
-    // Loop through each ACF field and categorize by data type
-    foreach ($acf_fields as $field_name => $field_value) {
-        if (is_string($field_value)) {
-            $fields_by_type['string'][$field_name] = $field_value;
-        } elseif (is_numeric($field_value)) {
-            $fields_by_type['number'][$field_name] = $field_value;
-        } elseif (is_array($field_value)) {
-            $fields_by_type['array'][$field_name] = $field_value;
-        }
-    }
-
-    return $fields_by_type;
-}
-
+// We're outside the loop, so grab the ID le scrappy way
 $current_url = esc_url($_SERVER['REQUEST_URI']);
 $post_id = url_to_postid($current_url);
-$fields = get_acf_fields_by_type($post_id);
+
+if (get_post_type($post_id) === 'counselor' && $post_id) {
+    function get_acf_fields_by_type($post_id = null) {
+        $acf_fields = get_fields($post_id); // Get all ACF fields for the post
+
+        $fields_by_type = array(
+            'string' => array(),
+            'number' => array(),
+            'array' => array(),
+        );
+
+        // Loop through each ACF field and categorize by data type
+        foreach ($acf_fields as $field_name => $field_value) {
+            if (is_string($field_value)) {
+                $fields_by_type['string'][$field_name] = $field_value;
+            } elseif (is_numeric($field_value)) {
+                $fields_by_type['number'][$field_name] = $field_value;
+            } elseif (is_array($field_value)) {
+                $fields_by_type['array'][$field_name] = $field_value;
+            }
+        }
+
+        return $fields_by_type;
+    }
+
+    $current_url = esc_url($_SERVER['REQUEST_URI']);
+    $post_id = url_to_postid($current_url);
+    $fields = get_acf_fields_by_type($post_id);
 
     // echo "<pre>";
     // print_r($fields);
@@ -68,20 +73,29 @@ $fields = get_acf_fields_by_type($post_id);
     
         return $list;
     }
+
+    $defaultImageURL = get_stylesheet_directory_uri() . '/assets/images/counselor--default-image.png';
     
-    $counselor_title = $fields['string']['counselor_title'];
-    $counselor_image = $fields['array']['counselor_image']['sizes']['large'];
-    $counselor_image_height = $fields['array']['counselor_image']['sizes']['large-height'];
-    $counselor_image_width = $fields['array']['counselor_image']['sizes']['large-width'];
-    $counselor_states = get_term_names_as_list($fields['array']['counselor_states']);
-    $counselor_specialization = get_term_names_as_list($fields['array']['counselor_specialization']);
-    $counselor_misc = $fields['string']['counselor_misc'];
+    $counselor_title = isset($fields['string']['counselor_title']) ? $fields['string']['counselor_title'] : '';
+    $counselor_image = isset($fields['array']['counselor_image']['sizes']['large']) ? $fields['array']['counselor_image']['sizes']['large'] : $defaultImageURL;
+    $counselor_image_height = isset($fields['array']['counselor_image']['sizes']['large-height']) ? $fields['array']['counselor_image']['sizes']['large-height'] : '';
+    $counselor_image_width = isset($fields['array']['counselor_image']['sizes']['large-width']) ? $fields['array']['counselor_image']['sizes']['large-width'] : '';
+    $counselor_states = isset($fields['array']['counselor_states']) ? get_term_names_as_list($fields['array']['counselor_states']) : '';
+    $counselor_specialization = isset($fields['array']['counselor_specialization']) ? get_term_names_as_list($fields['array']['counselor_specialization']) : '';
+    $counselor_misc = isset($fields['string']['counselor_misc']) ? $fields['string']['counselor_misc'] : '';
+    $counselor_schools_blount = isset($fields['array']['schools-blount']) ? get_term_names_as_list($fields['array']['schools-blount']) : '';
+    $counselor_schools_hamilton = isset($fields['array']['schools-hamilton']) ? get_term_names_as_list($fields['array']['schools-hamilton']) : '';
+    $counselor_schools_knox = isset($fields['array']['schools-knox']) ? get_term_names_as_list($fields['array']['schools-knox']) : '';
     
+
     $counties_in_tennessee = !empty($fields['array']['counties-in-tennessee']) ? get_term_names_as_list($fields['array']['counties-in-tennessee']) : '';
     $counties_in_california = !empty($fields['array']['counties-in-california']) ? get_term_names_as_list($fields['array']['counties-in-california']) : '';
     $counties_in_texas = !empty($fields['array']['counties-in-texas']) ? get_term_names_as_list($fields['array']['counties-in-texas']) : '';
     $counties_in_georgia = !empty($fields['array']['counties-in-georgia']) ? get_term_names_as_list($fields['array']['counties-in-georgia']) : '';
     $counties_in_north_carolina = !empty($fields['array']['counties-in-north-carolina']) ? get_term_names_as_list($fields['array']['counties-in-north-carolina']) : '';
+    $schools_in_blount = !empty($fields['array']['schools-blount']) ? get_term_names_as_list($fields['array']['schools-blount']) : '';
+    $schools_in_hamilton = !empty($fields['array']['schools-hamilton']) ? get_term_names_as_list($fields['array']['schools-hamilton']) : '';
+    $schools_in_knox = !empty($fields['array']['schools-knox']) ? get_term_names_as_list($fields['array']['schools-knox']) : '';
 
     function generate_counties_html($title, $content) {
         if (!empty($content)) {
@@ -93,6 +107,8 @@ $fields = get_acf_fields_by_type($post_id);
     
         return '';
     }
+}
+
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0","bottom":"0"}}},"className":"background-texture__excitement","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull background-texture__excitement" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"0","left":"0","right":"0"}}},"layout":{"type":"constrained","wideSize":"1500px"}} -->
@@ -127,310 +143,39 @@ $fields = get_acf_fields_by_type($post_id);
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:columns -->
+
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"level":4,"fontSize":"small"} -->
 <h4 class="wp-block-heading has-small-font-size"><strong>States</strong></h4>
 <!-- /wp:heading -->
-
 <!-- wp:list -->
 <?= $counselor_states; ?>
-
 </div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"level":4,"fontSize":"small"} -->
 <!-- /wp:heading -->
-
 <?php 
-
     echo !empty($counties_in_tennessee) ? generate_counties_html('Counties in Tennessee', $counties_in_tennessee) : '';
     echo !empty($counties_in_california) ? generate_counties_html('Counties in California', $counties_in_california) : '';
     echo !empty($counties_in_texas) ? generate_counties_html('Counties in Texas', $counties_in_texas) : '';
     echo !empty($counties_in_georgia) ? generate_counties_html('Counties in Georgia', $counties_in_georgia) : '';
     echo !empty($counties_in_north_carolina) ? generate_counties_html('Counties in Nort Carolina', $counties_in_north_carolina) : '';
-
 ?>
 </div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"level":4,"fontSize":"small"} -->
-<h4 class="wp-block-heading has-small-font-size"><strong>Schools in {County 1}</strong></h4>
 <!-- /wp:heading -->
-
-<!-- wp:list -->
-<ul><!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list -->
-
-<!-- wp:heading {"level":4,"fontSize":"small"} -->
-<h4 class="wp-block-heading has-small-font-size"><strong>Schools in {county2}</strong></h4>
-<!-- /wp:heading -->
-
-<!-- wp:list -->
-<ul><!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 1</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 2</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>School 3</li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
+<?php 
+    echo !empty($schools_in_knox) ? generate_counties_html('Schools in Knox County', $schools_in_knox) : '';
+    echo !empty($schools_in_blount) ? generate_counties_html('Schools in Blount County', $schools_in_blount) : '';
+    echo !empty($schools_in_hamilton) ? generate_counties_html('Schools in Hamilton County', $schools_in_hamilton) : '';
+?>
+</div>
+<!-- /wp:column -->
 <!-- /wp:columns --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
